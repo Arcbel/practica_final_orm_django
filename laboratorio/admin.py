7eeecd5a6ef5b54f9037831eccc41dd60ac9d08d
Valeria Rojas <valeria.rojasro@gmail.com>
2023-08-18 00:16:08 +0000

    primera view

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -3,12 +3,19 @@ from laboratorio.models import Laboratorio, DirectorGeneral, Producto
 
 class LaboratorioAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre_laboratorio')
+    ordering = ('id',)
 
 class DirectorGeneralAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre_director_general', 'laboratorio')
+    ordering = ('id',)
 
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre_producto', 'laboratorio', 'f_fabricacion', 'p_costo', 'p_venta')
+    list_display = ('id', 'nombre_producto', 'laboratorio', 'año_fabricacion', 'p_costo', 'p_venta')
+    ordering = ('id',)
+
+    def año_fabricacion(self, obj):
+        return obj.f_fabricacion.year
+    año_fabricacion.short_description = 'f fabricacion'
 
 admin.site.register(Laboratorio, LaboratorioAdmin)
 admin.site.register(DirectorGeneral, DirectorGeneralAdmin)
--- laboratorio/views.py
+++ laboratorio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+def v_index(request):
+    return render (request, 'index.html')
